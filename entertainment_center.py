1ce7850e1d1406fdf66c00ed380974e82739e9e3
Jackie Qi <superqichi@gmail.com>
2015-09-17 23:59:41 -0700

    display more info for movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -17,7 +17,9 @@ avengers2 = media.Movie("Avengers: Age of Ultron",
                         Heroes to stop the villainous Ultron from enacting \
                         his terrible plans.",
                         "https://upload.wikimedia.org/wikipedia/en/1/1b/Avengers_Age_of_Ultron.jpg",  # noqa
-                        "https://www.youtube.com/watch?v=bMP-FLmiIM0")
+                        "https://www.youtube.com/watch?v=bMP-FLmiIM0",
+                        "Joss Whedon",
+                        ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo"])
 
 san_andreas = media.Movie("San Andreas",
                           "In the aftermath of a massive earthquake in \
@@ -25,7 +27,10 @@ san_andreas = media.Movie("San Andreas",
                         dangerous journey with his ex-wife across \
                         the state in order to rescue his daughter.",
                           "https://upload.wikimedia.org/wikipedia/en/3/38/San_Andreas_poster.jpg",  # noqa
-                          "https://www.youtube.com/watch?v=yftHosO0eUo")
+                          "https://www.youtube.com/watch?v=yftHosO0eUo",
+                           "Brad Peyton",
+                          ["Dwayne Johnson", "Carla Gugino",
+                           "Alexandra Daddario"])
 
 edge_of_tomorrow = media.Movie("Edge of Tomorrow",
                                "A military officer is brought into an alien war \
@@ -35,14 +40,18 @@ edge_of_tomorrow = media.Movie("Edge of Tomorrow",
                             teams up with a Special Forces warrior to \
                             try and end the war.",
                                "https://upload.wikimedia.org/wikipedia/en/f/f9/Edge_of_Tomorrow_Poster.jpg",  # noqa
-                               "https://www.youtube.com/watch?v=bydHcLDfWuY")
+                               "https://www.youtube.com/watch?v=bydHcLDfWuY",
+                               "Doug Liman",
+                               ["Tom Cruise", "Emily Blunt", "Bill Paxton"])
 
 transformers_age_of_extinction = media.Movie(
     "Transformers: Age of Extinction",
     "Autobots must escape sight from a bounty hunter who \
                         has taken control of the human serendipity",
     "https://upload.wikimedia.org/wikipedia/en/9/94/Transformers_Age_of_Extinction_Poster.jpeg",  # noqa
-    "https://www.youtube.com/watch?v=DkIPIkPPbdo")
+    "https://www.youtube.com/watch?v=DkIPIkPPbdo",
+    "Michael Bay",
+    ["Mark Wahlberg", "Nicola Peltz", "Jack Reynor"])
 
 mission_impossible = media.Movie(
     "Mission: Impossible - Rogue Nation",
@@ -51,15 +60,19 @@ mission_impossible = media.Movie(
                         rogue organization as highly skilled as they are, \
                         committed to destroying the IMF.",
     "https://upload.wikimedia.org/wikipedia/en/f/fb/Mission_Impossible_Rogue_Nation_poster.jpg",  # noqa
-    "https://www.youtube.com/watch?v=pXwaKB7YOjw")
+    "https://www.youtube.com/watch?v=pXwaKB7YOjw",
+    "Christopher McQuarrie",
+    ["Tom Cruise", "Rebecca Ferguson", "Jeremy Renner"])
 
 steve_jobs = media.Movie("Steve Jobs: The Man in the Machine",
                          "A look at the personal and private life of the late \
                         Apple CEO, Steve Jobs.",
                          "https://upload.wikimedia.org/wikipedia/en/d/d0/Steve_Jobs_The_man_in_the_Machine.jpg",  # noqa
-                         "https://www.youtube.com/watch?v=SrlPyKxdMX4")
-movies = [avengers2, san_andreas, edge_of_tomorrow,
-          transformers_age_of_extinction,
-          mission_impossible, steve_jobs]
+                         "https://www.youtube.com/watch?v=SrlPyKxdMX4",
+                         "Alex Gibney",
+                         ["Steve Jobs"])
+movies = [san_andreas, transformers_age_of_extinction,
+          mission_impossible, steve_jobs, avengers2, edge_of_tomorrow]
+
 # call open_movies_page function in fresh_tomatoes.py which will load webpage
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -12,11 +12,13 @@ class Movie():
     """This class provides a way to store movie related information"""
 
     def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
+                 trailer_youtube, director, stars):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        self.director = director
+        self.stars = stars
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
